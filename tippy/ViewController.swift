38256bb3479dd561a9f564c87375599d7bb7bb9d
yelitzam <ymendez290@gmail.com>
2020-07-08 19:34:56 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Litza Mendez on 7/8/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
